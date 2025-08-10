8eb4d0a14bdab9e7c2a2293469ded94149704391
ECanDo <ecandoecantdo@gmail.com>
2025-08-09 20:55:32 -0600

    New rolling functions.
    One master dice roll function, to separate 2 dice rollers, one for a very large number of dice, one for a smaller amount.

--- dice_logic.py
+++ dice_logic.py
@@ -1,11 +1,38 @@
+# file dice_logic.py
+
 import random
+import hmac
+import hashlib
+import json
+import time
+from pathlib import Path
+from typing import Optional
 
 
-def roll_large_dice(num_dice, sides):
+def roll_large_dice(num_dice, sides) -> int:
     mean = num_dice * (sides + 1) / 2
     variance = num_dice * ((sides ** 2 - 1) / 12)
     stddev = variance ** 0.5
     return int(random.gauss(mean, stddev))
+
+
+def roll_small_dice(num_dice, sides) -> int:
+    sum = 0
+    for _ in range(num_dice):
+        sum += random.randint(1, sides)
+
+    return sum
+
+
+def roll_dice(num_dice, sides) -> int:
+    if num_dice < 1:
+        return -1
+
+    split = 10
+    if num_dice > split:
+        return roll_large_dice(num_dice, sides)
+
+    return roll_small_dice(num_dice, sides)
 
 
 def compute_roll(dice_number: int, dice_sides: int = 6):
