c96a69f6a1ee9fa7011c61de38b9494c1c65edf3
ECanDo <ecandoecantdo@gmail.com>
2025-08-10 20:02:48 -0600

    Forgot to update version number

--- src/script_updater.py
+++ src/script_updater.py
@@ -7,7 +7,7 @@ import subprocess
 GITHUB_API_RELEASES_URL = "https://api.github.com/repos/{owner}/{repo}/releases/latest"
 GITHUB_OWNER = "ECantDo"
 GITHUB_REPO = "DiceRolling"
-CURRENT_VERSION = "1.2.3"
+CURRENT_VERSION = "1.2.4"
 EXECUTABLE_NAME = "roller_app.exe"
 
 
